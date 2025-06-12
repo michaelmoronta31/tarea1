880b583916cfd7440e3adc10d4cf9f922c54d413
michaelmoronta31 <michaelmoronta31@gmail.com>
2025-06-11 20:28:16 -0400

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Inversiones_moronta.models;
+using System.Collections.Generic;
+
+namespace Inversiones_moronta.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Inversion> Inversiones { get; set; }
+    }
+}
+
---
+++ Inversion.cs
@@ -0,0 +1,12 @@
+﻿namespace Inversiones_moronta.models
+{
+    public class Inversion
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Activo { get; set; } 
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal PrecioCompra { get; set; }
+    }
+}
